6d371fe660c8a997c635dfa29ecbccdf17022427
metrovian <paramedoct@gmail.com>
2025-02-08 18:31:37 +0900

    LZ77 Header 구현

--- LosslessCompression/HeaderLZ77.h
+++ LosslessCompression/HeaderLZ77.h
@@ -1,1 +1,13 @@
 #pragma once
+#pragma pack(push, 1)
+struct HeaderLZ77
+{
+    uint16_t type = 0x5A4C;
+    uint32_t fsi;
+    uint32_t ofs = 0x0018;
+    int32_t width;
+    int32_t height;
+    uint16_t depth = 0x0018;
+    uint32_t dsi;
+};
+#pragma pack(pop)
