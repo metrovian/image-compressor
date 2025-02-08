3829996a222adb2f3e8baa97dae13dc90b2fc564
metrovian <paramedoct@gmail.com>
2025-02-08 21:51:06 +0900

    LZ78 Compression 작업 준비

---
+++ LosslessCompression/LZ78.cpp
@@ -0,0 +1,12 @@
+#include "LZ78.h"
+#include "HeaderLZ78.h"
+
+bool LZ78::decode(const std::string& _fname)
+{
+    return false;
+}
+
+bool LZ78::encode(const std::string& _fname)
+{
+    return false;
+}
