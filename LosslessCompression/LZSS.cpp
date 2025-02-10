a02a5b3197f9c1518aba809bd4bb4b356a4d2c8e
metrovian <paramedoct@gmail.com>
2025-02-10 22:24:42 +0900

    LZSS Compression 작업 준비

---
+++ LosslessCompression/LZSS.cpp
@@ -0,0 +1,12 @@
+#include "LZSS.h"
+#include "HeaderLZSS.h"
+
+bool LZSS::decode(const std::string& _fname)
+{
+    return false;
+}
+
+bool LZSS::encode(const std::string& _fname)
+{
+    return false;
+}
