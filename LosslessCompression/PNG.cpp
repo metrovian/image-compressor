22ca08b9db08a3516121e09b04686e003982939e
metrovian <paramedoct@gmail.com>
2025-02-15 20:54:05 +0900

    PNG Format 작업 준비

---
+++ LosslessCompression/PNG.cpp
@@ -0,0 +1,14 @@
+#include "PNG.h"
+#include "HeaderPNG.h"
+
+#include "zlib.h"
+
+bool PNG::decode(const std::string& _fname)
+{
+    return false;
+}
+
+bool PNG::encode(const std::string& _fname)
+{
+    return false;
+}
