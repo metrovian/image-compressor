3e554f4e88afec4155e53a44c10eca64ece43c8b
metrovian <paramedoct@gmail.com>
2025-03-05 22:22:10 +0900

    YUV Transform 구현

--- PerceptualCompression/HeaderYUV.h
+++ PerceptualCompression/HeaderYUV.h
@@ -9,8 +9,8 @@ struct HeaderYUV
     int32_t height;
     uint16_t depth = 0x0018;
     uint32_t dsi;
-    uint8_t qdy = 0x04;
-    uint8_t qdb = 0x02;
-    uint8_t qdr = 0x02;
+    uint8_t qdy = 0x0004;
+    uint8_t qdb = 0x0002;
+    uint8_t qdr = 0x0002;
 };
 #pragma pack(pop)
--- PerceptualCompression/YUV.cpp
+++ PerceptualCompression/YUV.cpp
@@ -2,7 +2,28 @@
 
 std::vector<uint8_t> YUV::transform(const std::vector<uint8_t>& _bmp)
 {
-    return std::vector<uint8_t>();
+    std::vector<uint8_t> ret = _bmp;
+
+    uint64_t pwidth = (width * 3 + 3) & ~3;
+
+    for (uint64_t i = 0; i < width; ++i)
+    {
+        for (uint64_t j = 0; j < height; ++j)
+        {
+            uint64_t bmpi = (height - j - 1) * pwidth + i * 3;
+            uint64_t yuvi = (j * width + i) * 3;
+
+            uint8_t scb = _bmp[bmpi + 0];
+            uint8_t scg = _bmp[bmpi + 1];
+            uint8_t scr = _bmp[bmpi + 2];
+            
+            ret[yuvi + 0] = static_cast<uint8_t>(16 + (0.257 * scr) + (0.504 * scg) + (0.098 * scb));
+            ret[yuvi + 1] = static_cast<uint8_t>(128 - (0.148 * scr) - (0.291 * scg) + (0.439 * scb));
+            ret[yuvi + 2] = static_cast<uint8_t>(128 + (0.439 * scr) - (0.368 * scg) - (0.071 * scb));
+        }
+    }
+
+    return ret;
 }
 
 std::vector<uint8_t> YUV::inverse(const std::vector<uint8_t>& _yuv)
