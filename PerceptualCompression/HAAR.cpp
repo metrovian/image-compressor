9bc18934d4ec1c15385644b8508809f1616d2583
metrovian <paramedoct@gmail.com>
2025-03-16 14:57:34 +0900

    DWT Compression 작업 준비

---
+++ PerceptualCompression/HAAR.cpp
@@ -0,0 +1,22 @@
+#include "HAAR.h"
+#include "HeaderHAAR.h"
+
+std::vector<uint8_t> HAAR::transform(const std::vector<uint8_t>& _bmp)
+{
+    return std::vector<uint8_t>();
+}
+
+std::vector<uint8_t> HAAR::inverse(const std::vector<uint8_t>& _dwt)
+{
+    return std::vector<uint8_t>();
+}
+
+bool HAAR::decode(const std::string& _fname)
+{
+    return false;
+}
+
+bool HAAR::encode(const std::string& _fname)
+{
+    return false;
+}
